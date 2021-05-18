1c3daf07445f07e6f6731dab83c764dce675d710
Juan G. Cabera <j.cabreraortiz@outlook.com>
2021-05-18 12:14:21 -0400

    Added Createsnake func and moving func

--- ProyectSnake.py
+++ ProyectSnake.py
@@ -33,3 +33,38 @@ snake=[0]*4
 snakegame=GraphWin('Snake Game', 600,600)
 createhabitat(snakegame)
 instructions(snakegame)
+
+
+
+
+def createsnake():
+    snake = [0]*4
+    size=4
+    for i in range(size):
+        snake[i] = Rectangle(Point(300-(i*15), 247),Point(300-(i*15)+14, 261))
+        snake[i].setFill("black")
+        snake[i].setOutline("red")
+        snake[i].draw(snakegame)
+
+    return snake
+
+
+def move(snake, k, snakegame):
+    max = size-1
+    snake[max].undraw()
+    
+    while max > 0:
+        snake[max] = snake[max-1]
+        max = max -1
+        
+    snake[0] = snake[1].clone()
+    if Direction == 1:
+        snake[0].move(15,0)
+    elif Direction == 2:
+        snake[0].move(0,15)
+    elif Direction == 3:
+        snake[0].move(-15,0)
+    elif Direction == 4:
+        snake[0].move(0,-15)
+
+    snake[0].draw(snakegame)
