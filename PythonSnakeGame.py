8ed2d1e41495a7692e33393550310c2b1639150c
Juan G. Cabera <j.cabreraortiz@outlook.com>
2021-05-22 12:05:12 -0400

    Snake game class

--- PythonSnakeGame.py
+++ PythonSnakeGame.py
@@ -27,7 +27,6 @@ def instructions(snakegame):
     click_msg.undraw()
     wel_msg.undraw()
 
-    
 size=4
 snake=[0]*4
 snakegame=GraphWin('Snake Game', 600,600)
@@ -35,65 +34,6 @@ createhabitat(snakegame)
 instructions(snakegame)
 
 
-
-
-def createsnake():
-    snake = [0]*4
-    size=4
-    for i in range(size):
-        snake[i] = Rectangle(Point(300-(i*15), 247),Point(300-(i*15)+14, 261))
-        snake[i].setFill("black")
-        snake[i].setOutline("red")
-        snake[i].draw(snakegame)
-
-    return snake
-
-
-def move(snake, k, snakegame):
-    max = size-1
-    snake[max].undraw()
-    
-    while max > 0:
-        snake[max] = snake[max-1]
-        max = max -1
-        
-    snake[0] = snake[1].clone()
-    if Direction == 1:
-        snake[0].move(15,0)
-    elif Direction == 2:
-        snake[0].move(0,15)
-    elif Direction == 3:
-        snake[0].move(-15,0)
-    elif Direction == 4:
-        snake[0].move(0,-15)
-
-    snake[0].draw(snakegame)
-
-
-def checkBounds(snake):
-    if (snake[0].getCenter().getX() > 540 or
-        snake[0].getCenter().getX() < 53 or
-        snake[0].getCenter().getY() < 59 or
-        snake[0].getCenter().getY() > 546):
-        return True
-
-    elif (snake[0].getCenter().getX() == Apple.getCenter().getX() and
-          snake[0].getCenter().getY() == Apple.getCenter().getY()):
-        newApple()
-        grow(snake)
-        
-        
-    for j in range(3, size):
-        if (snake[0].getCenter().getX() == snake[j].getCenter().getX() and
-            snake[0].getCenter().getY() == snake[j].getCenter().getY()):
-            return True
-
-
-def grow(snake):
-
-   snake.append(snake[size-3].clone())
-   snake.append(snake[size-3].clone())
-    
 def newApple():
     
     dx = (randint(0, 31))*15
@@ -111,6 +51,64 @@ def newApple():
     Apple.draw(snakegame)
 
 
+class Snake():
+    def __init__(tempsnake):
+        tempsnake.size = 4
+        tempsnake.snake = [0]*4
+    
+
+    def createsnake(tempsnake):
+        tempsnake.snake = [0]*4
+        tempsnake.size = 4
+        for i in range(tempsnake.size):
+            tempsnake.snake[i] = Rectangle(Point(300-(i*15), 247),Point(300-(i*15)+14, 261))
+            tempsnake.snake[i].setFill("cyan")
+            tempsnake.snake[i].draw(snakegame)
+
+
+    def grow(tempsnake):
+        tempsnake.size = tempsnake.size +2
+        tempsnake.snake.append(tempsnake.snake[tempsnake.size-3].clone())
+        tempsnake.snake.append(tempsnake.snake[tempsnake.size-3].clone())
+
+    def move(tempsnake, k, win):
+        max = tempsnake.size-1
+        tempsnake.snake[max].undraw()
+        
+        while max > 0:
+            tempsnake.snake[max] = tempsnake.snake[max-1]
+            max = max -1
+            
+        tempsnake.snake[0] = tempsnake.snake[1].clone()
+        if Direction == 1:
+            tempsnake.snake[0].move(15,0)
+        elif tempsnake == 2:
+            tempsnake.snake[0].move(0,15)
+        elif Direction == 3:
+            tempsnake.snake[0].move(-15,0)
+        elif Direction == 4:
+            tempsnake.snake[0].move(0,-15)
+
+        tempsnake.snake[0].draw(win)
+
+    def checkBounds(tempsnake):
+        if (tempsnake.snake[0].getCenter().getX() > 545 or
+            tempsnake.snake[0].getCenter().getX() < 60 or
+            tempsnake.snake[0].getCenter().getY() < 175 or
+            tempsnake.snake[0].getCenter().getY() > 545):
+            return True
+
+        elif (tempsnake.snake[0].getCenter().getX() == Apple.getCenter().getX() and
+              tempsnake.snake[0].getCenter().getY() == Apple.getCenter().getY()):
+            newApple()
+            tempsnake.grow()
+    
+            
+        for j in range(3, tempsnake.size):
+            if (tempsnake.snake[0].getCenter().getX() == tempsnake.snake[j].getCenter().getX() and
+                tempsnake.snake[0].getCenter().getY() == tempsnake.snake[j].getCenter().getY()):
+                return True
+
 
 def main():
 
@@ -119,29 +117,30 @@ def main():
     global Direction
     Direction=1
 
-    asnake=createsnake()
+    asnake=Snake()
+    asnake.createsnake()
     newApple()
     
   
     while(Lost!=True):
         k=snakegame.checkKey()
         time.sleep(0.10)
-        if checkBounds(asnake) == True:
-            Lost = True
+        if asnake.checkBounds() == True:
+            asnake.Lost = True
         elif ((k == "d" or k == "Right") and Direction!=3):
             Direction = 1
-            move(asnake, k, snakegame)
+            asnake.move( k, snakegame)
         elif ((k == "s" or k == "Down") and Direction!=4):
             Direction = 2
-            move(asnake,k, snakegame)
+            asnake.move(k, snakegame)
         elif ((k == "a" or k == "Left") and Direction!=1):
             Direction  = 3
-            move(asnake, k, snakegame)
+            asnake.move(k, snakegame)
         elif ((k == "w" or k == "Up") and Direction!=2):
             Direction  = 4
-            move(asnake, k, snakegame)
+            asnake.move(k, snakegame)
         else:
-            move(asnake, k, snakegame)
+            asnake.move(k, snakegame)
    
 
 
